f345aeb5f4022e3ae2c397c8c43b3445b6adbd02
Dries Van de Velde <driesvandevelde@icloud.com>
2021-04-19 22:24:05 +0200

    :construction: added controllers and routes

--- api/app/Models/recipe.php
+++ api/app/Models/recipe.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class recipe extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BasketController;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/basket', [BasketController::class, 'show']);
+Route::put('/recipes', [BasketController::class, 'like']);
+
+Route::resource('users', UserController::class);
+Route::resource('recipes', RecipeController::class);
+Route::resource('ingredients', IngredientController::class)->except(['index']);
