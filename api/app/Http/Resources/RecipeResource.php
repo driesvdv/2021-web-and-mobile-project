aada3271ef326f009f0fc686e7312692d08f1185
Dries Van de Velde <driesvandevelde@icloud.com>
2021-05-29 21:11:23 +0200

    Added recipe resource

---
+++ api/app/Http/Resources/RecipeResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RecipeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "imageUrl" => $this->imageUrl,
+            "likes" => $this->likes,
+            "dislikes" => $this->dislikes,
+            "created_at" => $this->created_at,
+            "reactions" => ReactionResource::collection($this->reactions),
+            "ingredients" => IngredientResource::collection($this->ingredients),
+            "steps" => StepResource::collection($this->steps),
+        ];
+    }
+}
