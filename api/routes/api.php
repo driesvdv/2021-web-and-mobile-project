ae5d3b2d9571fe15648558253fcaaacea0b20579
Dries Van de Velde <driesvandevelde@icloud.com>
2021-05-29 14:55:28 +0200

    Add auth

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\IngredientController;
 use App\Http\Controllers\RecipeController;
@@ -18,13 +19,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function(Request $request) {
+        return auth()->user();
+    });
+
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
+
+Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('auth/login', [AuthController::class, 'login']);
 
 Route::get('/basket', [BasketController::class, 'show']);
 Route::put('/recipes', [BasketController::class, 'like']);
 
-Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)->except(['index']);
 Route::resource('recipes', RecipeController::class);
 Route::resource('ingredients', IngredientController::class)->except(['index']);
+
+
