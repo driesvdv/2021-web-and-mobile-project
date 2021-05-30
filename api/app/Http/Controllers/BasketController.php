37ec6ca33dd89f86163883dc03f08f488c4a4f94
Dries Van de Velde <driesvandevelde@icloud.com>
2021-05-30 17:23:56 +0200

    Added basket calc functionallity

--- api/app/Http/Controllers/BasketController.php
+++ api/app/Http/Controllers/BasketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BasketResource;
 use App\Traits\ApiResponser;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -10,6 +11,18 @@ class BasketController extends Controller
 {
     use ApiResponser;
 
+    private function calc($ingredients)
+    {
+        $unique = $ingredients->unique("name");
+
+        $filtered = $unique->flatMap(function ($ingredient) {
+            return [$ingredient->name => ["unit" => $ingredient->unit, "amount" => 0]];
+        })->toArray();
+
+        foreach($ingredients as $ingredient) $filtered[$ingredient->name]["amount"] += $ingredient->pivot->amount;
+
+        return $filtered;
+    }
 
     public function index()
     {
@@ -17,17 +30,25 @@ class BasketController extends Controller
             ->plannedRecipes()
             ->wherePivot('date', '>=', Carbon::now()->startOfDay())
             ->wherePivot('date', '<', Carbon::now()->addWeeks(2)->startOfDay())
-            ->limit(10)
             ->with('ingredients')
+            ->limit(10)
             ->get();
 
-        return $plannedRecipes;
+        $ingredients = $plannedRecipes->flatMap(function ($recipe) {
+            return $recipe->ingredients;
+        });
+
+        return $this->calc($ingredients);
+
+
+        return $ingredients;
+        return BasketResource::collection();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -38,7 +59,7 @@ class BasketController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -49,8 +70,8 @@ class BasketController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -61,7 +82,7 @@ class BasketController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
---
+++ api/app/Http/Resources/BasketResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BasketResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "name" => $this->name,
+            "unit" => $this->unit,
+            "amount" => $this->pivot->amount,
+        ];
+    }
+}
