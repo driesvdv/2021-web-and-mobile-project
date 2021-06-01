94e1170a1f5f0c41932abe802670e3992e07f12f
Dries Van de Velde <driesvandevelde@icloud.com>
2021-06-01 16:30:41 +0200

    Finished base overview page

--- VirtualCook/components/recipes/RecipeComponent.js
+++ VirtualCook/components/recipes/RecipeComponent.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
-const RecipeComponent = () => {
+const RecipeComponent = ({ recipe }) => {
+
+  useEffect(() => {
+    console.log(recipe);
+  });
+
+  const moreInfo = (id) => {
+    Alert.alert(`Meer info ${id}`)
+  }
+
   return (
     <View style={styles.cardStyle}>
       <Image
         style={styles.imageStyle}
         source={require("./../../assets/images/defaultMeal.jpg")}
       />
-      <Text>Titel</Text>
+      <View style={styles.bottomStyle}>
+        <Text style={styles.titleStyle}>{recipe.name}</Text>
+      </View>
+      <Pressable style={styles.infoButtonStyle} onPress={() => moreInfo(recipe.id)}>
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 16 }}>Meer info</Text>
+      </Pressable>
     </View>
   );
 };
@@ -17,12 +32,11 @@ const RecipeComponent = () => {
 const styles = StyleSheet.create({
   cardStyle: {
     backgroundColor: "white",
-    height: 350,
     marginHorizontal: 20,
     marginVertical: 10,
     borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -34,9 +48,31 @@ const styles = StyleSheet.create({
   },
   imageStyle: {
     height: 200,
-    width: '100%',
+    width: "100%",
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
+  },
+  titleStyle: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginLeft: '2.5%',
+    marginVertical: '5%',
+  },
+  bottomStyle: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  infoButtonStyle: {
+    fontFamily: "Roboto",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#FF9046",
+    width: "95%",
+    height: 50,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    marginLeft: '2.5%',
   }
 });
 
