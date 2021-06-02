5888c02e294edfbb94e826b6bb2e8d86bee473e4
Dries Van de Velde <driesvandevelde@icloud.com>
2021-06-02 15:40:41 +0200

    Added logout

--- VirtualCook/screens/Home/HomeScreen.js
+++ VirtualCook/screens/Home/HomeScreen.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import axiosInstance from "../../utils/axiosInstance";
 import AddIngredientCtaComponent from "../../components/recipes/AddIngredientCTAComponent";
 import RecipeComponent from "../../components/recipes/RecipeComponent";
+import Logout from "../../assets/icons/logout.svg";
+import { AuthContext } from "../../utils/authContext";
 
 export const HomeScreen = ({ navigation }) => {
   const [recipes, setRecipes] = useState(null);
   const [reload, setReload] = useState(false);
+
+  const { signOut } = useContext(AuthContext);
 
   useEffect(() => {
     axiosInstance.get("/recipes")
@@ -19,7 +23,12 @@ export const HomeScreen = ({ navigation }) => {
 
   return (
     <View style={{ marginTop: 30, marginBottom: 155 }}>
-      <Text style={styles.header}>Overzicht</Text>
+      <View style={{ flexDirection: 'row', justifyContent: "space-between", alignContent: "center"}}>
+        <Text style={styles.header}>Overzicht</Text>
+        <Pressable style={styles.logoutButton} onPress={() => signOut()}>
+          <Logout height={30} width={30}/>
+        </Pressable>
+      </View>
       <AddIngredientCtaComponent navigation={navigation}/>
       <FlatList
         data={recipes}
@@ -48,5 +57,21 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     justifyContent: "center",
     alignItems: "center",
+  },
+  logoutButton: {
+    marginRight: "5%",
+    backgroundColor: "white",
+    padding: 10,
+    height: 50,
+    width: 50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    elevation: 24,
+    borderRadius: 10,
   },
 });
